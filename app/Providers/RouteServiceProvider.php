885a3da9627a20729fb6317ae868137ca9bc482d
vicpcr <134752980+vicpcr@users.noreply.github.com>
2023-10-17 10:33:13 -0600

    version 2.2

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/asesorias.index';
+    public const HOME = '/asesorias';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
